a62ca3e0364e3f37e2c4b664f655c3ec6b74b590
carmelariadas <carmen.mk.167@gmail.com>
2023-02-19 17:05:15 +0000

    seccion eventos arreglada con scroll

--- src/front/js/component/Jumbotron.js
+++ src/front/js/component/Jumbotron.js
@@ -5,11 +5,11 @@ export const  Jumbotron= () => {
   return (
     <div className='container-fluid'>
         <div className='row jumbotron'>
-          <div className='col-sm-2 col-md-3'></div>
-            <div className='col-sm-8 col-md-6  text-center'>
+          <div className='col-sm-1 col-xl-4'></div>
+            <div className='col-sm-10 col-xl-4  text-center'>
             Conoce la música en vivo de tu localidad y conecta con músicos
             </div>
-            <div className='col-sm-2 col-md-3'></div>
+            <div className='col-sm-1 col-xl-4'></div>
         </div>
     </div>
   )
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,22 +6,22 @@ import { CardConcert } from "../component/CardConcert";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
-//   const concerts = [
-//     {
-//       name: "nombre1",
-//       description: "description1",
-//       event_profile_img: "url",
-//       establishment_name: "nombre del local",
-//       music_genre_event: [1, 2, 3],
-//     },
-//     {
-//       name: "nombre2",
-//       description: "description2",
-//       event_profile_img: "url",
-//       establishment_name: "nombre del local",
-//       music_genre_event: [1, 2, 3],
-//     },
-//   ];
+  //   const concerts = [
+  //     {
+  //       name: "nombre1",
+  //       description: "description1",
+  //       event_profile_img: "url",
+  //       establishment_name: "nombre del local",
+  //       music_genre_event: [1, 2, 3],
+  //     },
+  //     {
+  //       name: "nombre2",
+  //       description: "description2",
+  //       event_profile_img: "url",
+  //       establishment_name: "nombre del local",
+  //       music_genre_event: [1, 2, 3],
+  //     },
+  //   ];
 
   // console.log(store.concerts)
   return (
@@ -29,18 +29,18 @@ export const Home = () => {
       <Jumbotron />
       <div className="container-fluid seccioneshome">
         <h2>Conciertos</h2>
-      </div>
-      <div className="d-flex overflow-auto py-5 rowCards">
-        {store.concerts?.map((element) => (
-			
-          <CardConcert
-            name={element.name}
-            description={element.description}
-            event_profile_img={element.event_profile_img}
-            establishment_name={element.establishment_name}
-            music_genre_event={element.music_genre_event}
-          />
-        ))}
+
+        <div className="rowCards">
+          {store.concerts?.map((element) => (
+            <CardConcert
+              name={element.name}
+              description={element.description}
+              event_profile_img={element.event_profile_img}
+              establishment_name={element.establishment_name}
+              music_genre_event={element.music_genre_event}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
